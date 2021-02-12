643c236164fe6f412891089141c053fb9a7d6c21
Erik Butcher <with_a_k@mac.com>
2021-02-11 17:40:25 -0700

    Vanilla Todo, commit 2

--- js/todo/todo-item.js
+++ js/todo/todo-item.js
@@ -1,9 +1,12 @@
-class TodoItem {
-  constructor(title, index, complete = false) {
+export class TodoItem {
+  constructor(title, index, target, complete = false) {
     this.title = title;
     this.index = index;
     this.complete = complete;
     this.existing = false;
+    if (this.title === "") {
+      return;
+    }
     this.render(target);
   }
 
@@ -13,13 +16,14 @@ class TodoItem {
   }
 
   render(target) {
-    let item = document.createElement(div);
+    let item = document.createElement('div');
     item.id = `${this.title}-${this.index}`;
     item.classList.add('todoItem');
-    let confirm = document.createElement(button);
+    let confirm = document.createElement('button');
     confirm.id = `complete-${this.title}-${this.index}`;
+    confirm.innerHTML = "Finished?";
     confirm.onclick = this.crossOut.bind(this);
-    let title = document.createElement(h5);
+    let title = document.createElement('h5');
     title.innerHTML = this.title;
     title.classList.add('todoTitle');
     //appendChild returns the child, so these can't be chained
---
+++ js/todo/todo.js
@@ -0,0 +1,12 @@
+import TodoItem from './todo-item.js';
+
+let TodoItemFactory = {
+  index: 0,
+  makeTodoItem: function(e) {
+    let title = document.querySelector('#item-name').value;
+    new TodoItem(title, index, document.querySelector('#list-display'));
+    this.index++;
+  }
+}
+
+document.querySelector('#add-item').addEventListener('touchend', TodoItemFactory.makeTodoItem);
