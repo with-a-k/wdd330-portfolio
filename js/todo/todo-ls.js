ada2022b757506b0a5ccbffe72597b743d862763
Erik Butcher <with_a_k@mac.com>
2021-02-13 21:15:31 -0700

    Save tdl as long string instead of JSON

--- js/todo/todo-ls.js
+++ js/todo/todo-ls.js
@@ -1,5 +1,5 @@
 function loadList() {
-  let todoList = localStorage.getItem('saved-tdl');
+  let todoList = localStorage.getItem('saved-tdl').split(";;");
   console.log(todoList);
   todoList.forEach(function(item) {
     console.log(item);
@@ -10,11 +10,9 @@ function saveList() {
   console.log('saveList was invoked');
   let todoList = Array.from(document.getElementById("list-display").children)
     .map((item) => {
-      return {
-        name: Array.from(item.children)[0].innerHTML,
-        timestamp: item.id.substr(item.id.indexOf('|')+1)
-      };
-    });
+      return item.id
+    })
+    .join(';;');
   console.log(todoList);
   localStorage.setItem('saved-tdl', todoList);
 }
